1af29a878b260124b10d8bc295e4d41d6e6f159f
Iris <mdza.iris@gmail.com>
2017-03-15 13:33:25 -0700

    Added sections to front page

--- themes/catching-the-spirit/front-page.php
+++ themes/catching-the-spirit/front-page.php
@@ -9,21 +9,41 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <div class="front-page-hero">
-                <?php echo CFS()->get('front_page_hero_image'); ?>
-            </div>
-            <div class="front-page-mission">
-                <?php echo CFS()->get('front_page_our_mission'); ?>
-            </div>
-            <div class="front-page-about">
-                <?php echo CFS()->get('front_page_who_we_are'); ?>
-            </div>
-            <div class="for-youth-banner">
-            </div>
-            <div class="front-page-programs">
-            </div>
-            <div class="front-page-programs-free">
-                <?php echo CFS()->get('front_page_programs_free'); ?>
+            <div class="mobile-container">
+				<div class="missin-about-wrapper">
+					<div class="front-page-hero">
+					</div>
+					<div class="front-page-mission">
+						<h2>Our mission</h2>
+						<p><?php echo CFS()->get('front_page_our_mission'); ?></p>
+					</div>
+					<div class="front-page-about">
+						<h2>Who we are?</h2>
+						<p><?php echo CFS()->get('front_page_who_we_are'); ?></p>
+					</div>
+				</div>
+				<a href="" class="blue-btn">Learn More<a>
+                <div class="for-youth-banner">
+                </div>
+                <div class="front-page-programs">
+					<h2>What do we offer?</h2>
+                </div>
+                <div class="front-page-programs-free">
+					<h2>Our programs are <span class="uppercase">free</span></h2>
+                    <p><?php echo CFS()->get('front_page_programs_free'); ?></p>
+					<a href="" class="red-btn">Volunteer<a>
+					<a href="" class="red-btn">Give<a>
+                </div>
+				<div class="front-page-social-media">
+					<h2>Follow us on Social Media!</h2>
+				</div>
+				<div class="front-page-testimonials">
+					<h2>Testimonials</h2>
+					<a href="" class="blue-btn">See More<a>
+				</div>
+				<div class="front-page-sponsors">
+					<h2 class="uppercase">A big thanks to our sponsors</h2>
+				</div>
             </div>
 
 		<?php if ( have_posts() ) : ?>
