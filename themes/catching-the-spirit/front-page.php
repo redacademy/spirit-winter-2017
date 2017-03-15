c9f96ce04c7b7195c47718cc5294c8a2b27bd4f4
Iris <mdza.iris@gmail.com>
2017-03-15 10:29:03 -0700

    add custom fields to front page

--- themes/catching-the-spirit/front-page.php
+++ themes/catching-the-spirit/front-page.php
@@ -9,6 +9,22 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+            <div class="front-page-hero">
+                <?php echo CFS()->get('front_page_hero_image'); ?>
+            </div>
+            <div class="front-page-mission">
+                <?php echo CFS()->get('front_page_our_mission'); ?>
+            </div>
+            <div class="front-page-about">
+                <?php echo CFS()->get('front_page_who_we_are'); ?>
+            </div>
+            <div class="for-youth-banner">
+            </div>
+            <div class="front-page-programs">
+            </div>
+            <div class="front-page-programs-free">
+                <?php echo CFS()->get('front_page_programs_free'); ?>
+            </div>
 
 		<?php if ( have_posts() ) : ?>
 
