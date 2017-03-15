f00ab2fa5e032ffe5be7e76ce035cc6890c5c8e5
Iris <mdza.iris@gmail.com>
2017-03-15 16:13:38 -0700

    styled green boxes on front page

--- themes/catching-the-spirit/front-page.php
+++ themes/catching-the-spirit/front-page.php
@@ -9,44 +9,43 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <div class="mobile-container">
-				<div class="missin-about-wrapper">
-					<div class="front-page-hero">
-					</div>
-					<div class="front-page-mission">
-						<h2>Our mission</h2>
-						<p><?php echo CFS()->get('front_page_our_mission'); ?></p>
-					</div>
-					<div class="front-page-about">
-						<h2>Who we are?</h2>
-						<p><?php echo CFS()->get('front_page_who_we_are'); ?></p>
-					</div>
-				</div>
-				<a href="" class="blue-btn">Learn More<a>
-                <div class="for-youth-banner">
-                </div>
-                <div class="front-page-programs">
-					<h2>What do we offer?</h2>
-                </div>
-                <div class="front-page-programs-free">
-					<h2>Our programs are <span class="uppercase">free</span></h2>
-                    <p><?php echo CFS()->get('front_page_programs_free'); ?></p>
-					<a href="" class="red-btn">Volunteer<a>
-					<a href="" class="red-btn">Give<a>
-                </div>
-				<div class="front-page-social-media">
-					<h2>Follow us on Social Media!</h2>
-				</div>
-				<div class="front-page-testimonials">
-					<h2>Testimonials</h2>
-					<a href="" class="blue-btn">See More<a>
-				</div>
-				<div class="front-page-sponsors">
-					<h2 class="uppercase">A big thanks to our sponsors</h2>
-				</div>
-            </div>
+			<div class="front-page-hero">
+			</div>
+			<div class="front-page-mission">
+				<h2>Our mission</h2>
+				<p><?php echo CFS()->get('front_page_our_mission'); ?></p>
+			</div>
+			<div class="front-page-about">
+				<h2>Who we are?</h2>
+				<p><?php echo CFS()->get('front_page_who_we_are'); ?></p>
+				<a href="" class="blue-btn">Learn More</a>
+			</div>
+			<div class="green-box for-youth-yellow">
+			</div>
+			<div class="front-page-programs">
+				<h2>What do we offer?</h2>
+			</div>
+			<div class="front-page-programs-free green-box">
+				<h2>Our programs are <span class="uppercase">free</span></h2>
+				<p><?php echo CFS()->get('front_page_programs_free'); ?></p>
+				<a href="" class="red-btn">Volunteer</a>
+				<a href="" class="red-btn">Give</a>
+			</div>
+			<div class="front-page-social-media">
+				<h2>Follow us on social media!</h2>
+				<i class="fa fa-facebook" aria-hidden="true"></i>
+				<i class="fa fa-instagram" aria-hidden="true"></i>
+				<i class="fa fa-twitter" aria-hidden="true"></i>
+			</div>
+			<div class="front-page-testimonials green-box">
+				<h2>Testimonials</h2>
+				<a href="" class="blue-btn">See More</a>
+			</div>
+			<div class="front-page-sponsors">
+				<h2>A big thanks to our sponsors</h2>
+			</div>
 
-		<?php if ( have_posts() ) : ?>
+		<!--<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
@@ -67,7 +66,7 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		<?php endif; ?>
+		<?php endif; ?>-->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
