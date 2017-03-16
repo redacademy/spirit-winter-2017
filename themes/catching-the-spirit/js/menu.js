1cae16db35b9a58a3f5c14b8417620c889cd1d55
Shirley <ma_shirley@msn.com>
2017-03-16 16:33:59 -0700

    submenu desktop wip.

--- themes/catching-the-spirit/js/menu.js
+++ themes/catching-the-spirit/js/menu.js
@@ -1,7 +1,18 @@
 (function($) {
-
+    // nav mobile
     $(".responsive-menu").hide();
     $(".menu-toggle").on("click", function(){
         $(".responsive-menu").toggle(600);
     });
+    // nav desktop
+    $(".sub-menu").hide();
+    $('.menu-item').hover(function() {
+    //show its submenu
+    $('ul', this).fadeIn(100);
+
+    }, function() {
+    //hide its submenu
+    $('ul', this).fadeOut(100);
+    });
+
 })(jQuery);
