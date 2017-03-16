d4b7e9867b6b024e3a40a59d39a032a8a44013c0
MisterPeddler <mr.padli@gmail.com>
2017-03-15 21:23:29 -0700

    added Sponsors page

--- themes/catching-the-spirit/page-sponsors.php
+++ themes/catching-the-spirit/page-sponsors.php
@@ -7,6 +7,25 @@
 
 get_header(); ?>
 
-	this is Privacy Policy
+<h1 class="page-title">Sponsors<h1>
+
+<h1 class="sponsor-thanks">Thank You</h1>
+
+<p><?php CFS()->get( 'sponsor-thanks' ); ?></p>
+
+<ul class="sponsors-page-list">
+
+    <?php $fields = CFS()->get('sponsors_list'); ?>
+    <?php foreach ( $fields as $key=>$field ): ?>
+        <li>
+            <img src="<?php echo $field['sponsor_logo']; ?>"/>
+            <p class="sponsor-name"><?php echo $field['sponsor_name']; ?></p>
+
+                <?php if($key === 0  && isset($field['sponsor_statement'])): ?>
+                <p><?php echo $field['sponsor_statement']; ?>
+                <?php endif ?>
+        </li>
+    <?php endforeach ?>
+</ul>
 
 <?php get_footer(); ?>
