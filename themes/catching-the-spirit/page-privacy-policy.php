52be2107b324dc0a72f9b69f42f0ba7736d5b002
MisterPeddler <mr.padli@gmail.com>
2017-03-17 10:03:45 -0700

    pulling before push for Shirley to test mobile

--- themes/catching-the-spirit/page-privacy-policy.php
+++ themes/catching-the-spirit/page-privacy-policy.php
@@ -7,6 +7,41 @@
 
 get_header(); ?>
 
+<h1 class="page-title">Privacy Policy</h1>
 
+  <?php $fields = CFS()->get('privacy_policy_loop'); ?>
+
+<div class="privacy-jump-to">
+
+	<h2 class="strong">Jump to a Section</h2>
+
+<ul class="jump-to-list">
+    <?php foreach ( $fields as $field ): ?>
+        <li>
+            <a href="#<?php echo str_replace(' ', '-', strtolower($field['privacy_policy_section_title'])) ?>">
+	&bull; <?php echo $field['privacy_policy_section_title']; ?>
+			</a>
+        </li>
+    <?php endforeach ?>
+</ul>
+
+
+</div>
+
+
+<div class="privacy-content">
+
+<ul class="privacy-content-list">
+    <?php foreach ( $fields as $field ): ?>
+        <li id="<?php echo str_replace(' ', '-', strtolower($field['privacy_policy_section_title'])) ?>">
+          <p><?php echo $field['privacy_policy_section_title'];?></p>
+		  <p><?php echo $field['privacy_policy_section_content'];?></p>
+		  <a href="#page" class="back-to-top">Back to Top</a>
+        </li>
+    <?php endforeach ?>
+</ul>
+
+
+</div>
 
 <?php get_footer(); ?>
