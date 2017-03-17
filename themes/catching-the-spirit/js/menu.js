277b814b4bfd32f6ed5bc4ce7a4f66350a374ab1
Shirley <ma_shirley@msn.com>
2017-03-16 22:44:34 -0700

    $ hide and show: logo and phone when click on icon menu.

--- themes/catching-the-spirit/js/menu.js
+++ themes/catching-the-spirit/js/menu.js
@@ -1,11 +1,13 @@
 (function($) {
     // nav mobile
-    $(".responsive-menu").hide();
-    $(".menu-toggle").on("click", function(){
-        $(".responsive-menu").toggle(600);
+    $('.responsive-menu').hide(); // hide nav
+    $('.menu-toggle').on('click', function(){
+        $('.header-logo').toggle(); // hide hide and show logo
+        $('.fa-phone').toggle(); // hide and show phone
+        $('.responsive-menu').toggle(); //hide and show nav
     });
     // nav desktop
-    $(".sub-menu").hide();
+    $('.sub-menu').hide();
     $('.menu-item').hover(function() {
     //show its submenu
     $('ul', this).fadeIn(100);
