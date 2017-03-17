62d79eb76824fa03626a98fc9bc25baee8ca346a
MisterPeddler <mr.padli@gmail.com>
2017-03-17 16:29:54 -0700

    added content to camp locations

--- themes/catching-the-spirit/page-camp-locations.php
+++ themes/catching-the-spirit/page-camp-locations.php
@@ -7,8 +7,43 @@
 
 get_header(); ?>
 
-this is camp locations
+<h1 class="page-title">Camp Locations</h1>
 
+<div class="multi-map">
 <?php echo do_shortcode('[put_wpgm id=1]') ?>
+</div>
+
+<?php $fields = CFS()->get('camp_locations_loop'); ?>
+
+<div class="camp-list-jump-to">
+
+	<h2 class="strong">Jump to a Camp</h2>
+
+<ul class="jump-to-list-camp">
+    <?php foreach ( $fields as $field ): ?>
+        <li>
+            <a href="#<?php echo str_replace(' ', '-', strtolower($field['camp_location_name'])) ?>">
+	        &bull; <?php echo $field['camp_location_name']; ?>
+			</a>
+        </li>
+    <?php endforeach ?>
+</ul>
+
+
+</div>
+
+<ul class="camp-locations-list">
+    <?php foreach ( $fields as $field ): ?>
+        <li class="camp-location-entry" id="<?php echo str_replace(' ', '-', strtolower($field['camp_location_name'])) ?>">
+          <p class="camp-location-name"><?php echo $field['privacy_policy_section_title'];?></p>
+		  <p><?php echo $field['camp_location_description'];?></p>
+		  <div class="google-map"><?php echo $field['camp_location_map'];?></div>
+		  <a href="#page" class="back-to-top">Back to Top</a>
+        </li>
+    <?php endforeach ?>
+</ul>
+
+
+
 
 <?php get_footer(); ?>
