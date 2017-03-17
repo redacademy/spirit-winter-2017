69923e15f9b5cdeda5f2ca66d1a6ab19166f1871
MisterPeddler <mr.padli@gmail.com>
2017-03-17 14:23:35 -0700

    edited membership

--- themes/catching-the-spirit/page-membership.php
+++ themes/catching-the-spirit/page-membership.php
@@ -7,6 +7,15 @@
 
 get_header(); ?>
 
-	this is page-membership
+	<h1 class="page-title"> Membership </h1>
+
+	<img src="<?php echo CFS()->get( 'membership_hero_image' ); ?>"/>
+
+	<p><?php echo CFS()->get( 'membership_main_text' ); ?></p>
+
+	<a class="red-btn">Join</a>
+
+	<p><?php echo CFS()->get( 'membership_fee_text' ); ?></p>
+
 
 <?php get_footer(); ?>
