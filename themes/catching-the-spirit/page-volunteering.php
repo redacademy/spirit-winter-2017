c7d2fe52b7a58021850d239978311fbb4b02c4f7
Shirley <ma_shirley@msn.com>
2017-03-19 13:43:49 -0700

    pages.php our people and volunteering created on local.

---
+++ themes/catching-the-spirit/page-volunteering.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Volunteering Template
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+	
+
+        </main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
