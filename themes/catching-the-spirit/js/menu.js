5086394881c3b0717ed4eaaa32459094a16c6172
Shirley <ma_shirley@msn.com>
2017-03-19 12:04:39 -0700

    nav mobile $ working.

--- themes/catching-the-spirit/js/menu.js
+++ themes/catching-the-spirit/js/menu.js
@@ -1,21 +1,32 @@
 (function($) {
-    // nav mobile
+
+    if(('ontouchstart' in window)){//check for touch device
+        $('.sub-menu').hide();
+        $('.menu-item').click(function() { 
+            if ($(this).children('ul,p').is(':hidden') == true) {
+            $(this).children('ul,p').slideDown('slow');
+            return false
+            } 
+    });
+    }
+    else{
+        $('.sub-menu').hide();
+        $('.menu-item').hover(function() {
+        //show its submenu
+        $('ul', this).fadeIn(100);
+
+        }, function() {
+        //hide its submenu
+        $('ul', this).fadeOut(100);
+        });
+    }
+
+    // nav mobile toggle 
     $('.responsive-menu').hide(); // hide nav
     $('.menu-toggle').on('click', function(){
         $('.header-logo').toggle(); // hide hide and show logo
         $('.fa-phone').toggle(); // hide and show phone
         $('.responsive-menu').toggle(); //hide and show nav
-    });
-    
-    // nav desktop
-    $('.sub-menu').hide();
-    $('.menu-item').hover(function() {
-    //show its submenu
-    $('ul', this).fadeIn(100);
-
-    }, function() {
-    //hide its submenu
-    $('ul', this).fadeOut(100);
     });
 
 })(jQuery);
