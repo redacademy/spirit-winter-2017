13a8f3696828b905fd9037e7ee81651cb4a973cf
Shirley <ma_shirley@msn.com>
2017-03-19 16:50:42 -0700

    work on our people page commit before merge

--- themes/catching-the-spirit/page-our-people.php
+++ themes/catching-the-spirit/page-our-people.php
@@ -9,6 +9,13 @@ get_header(); ?>
 
     <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+            <h1>Our People</h2>
+
+            <div class="youth-council">
+                <h2>Youth Council</h2>
+                <p><?php echo CFS()->get('our_people_youth_council'); ?></p>
+            </div>
+            
 	
 
         </main><!-- #main -->
