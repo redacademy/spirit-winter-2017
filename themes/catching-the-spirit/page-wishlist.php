fd14082fd2990fc24e5dc739f89c54a347453c1b
MisterPeddler <mr.padli@gmail.com>
2017-03-20 11:05:39 -0700

    built and styled wishlist page

--- themes/catching-the-spirit/page-wishlist.php
+++ themes/catching-the-spirit/page-wishlist.php
@@ -7,6 +7,25 @@
 
 get_header(); ?>
 
+<h1 class="page-title">Our Wishlist</h1>
 
+<p> <?php echo CFS()->get('wishlist_opening_text');?> </p>
+
+<?php $fields = CFS()->get('wishlist_loop'); ?>
+
+
+<ul class="wishlist-loop">
+    <?php foreach ( $fields as $field ): ?>
+        <li class="wishlist-entry green-box">
+        <p class ="strong wishlist-heading"> <?php echo $field['wishlist_category'];?> </p>
+        <p><?php echo $field['wishlist_item_1'];?></p>
+        <p><?php echo $field['wishlist_item_2'];?></p>
+        <p><?php echo $field['wishlist_item_3'];?></p>
+        <p><?php echo $field['wishlist_item_4'];?></p>
+        </li>
+    <?php endforeach ?>
+</ul>
+
+<p> <?php echo CFS()->get('wishlist_closing_text')?> </p>
 
 <?php get_footer(); ?>
