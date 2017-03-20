35697d88cde38c54c320fd2a3085c047bd571fda
Iris <mdza.iris@gmail.com>
2017-03-20 12:44:01 -0700

    styled resources conversations page

--- themes/catching-the-spirit/page-resources-conversations.php
+++ themes/catching-the-spirit/page-resources-conversations.php
@@ -10,11 +10,17 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<header>
+				<h1 class="page-title"><?php echo CFS()->get('resource_page_title'); ?></h1>
+				<p><?php echo CFS()->get('resource_page_description'); ?></p>
+			</header>
+			<section>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
+				<?php endwhile; // End of the loop. ?>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
