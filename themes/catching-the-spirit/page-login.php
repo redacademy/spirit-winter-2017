a20a816b0104ef896fb6082d9af7f7be60fde9b6
Shirley <ma_shirley@msn.com>
2017-03-21 15:23:30 -0700

    login page style in progress

--- themes/catching-the-spirit/page-login.php
+++ themes/catching-the-spirit/page-login.php
@@ -1,9 +1,20 @@
 <?php
-$args = array(
-    'redirect' => home_url(), 
-    'id_username' => 'user',
-    'id_password' => 'pass',
-   ) 
-;?>
+if ( ! is_user_logged_in() ) { // Display WordPress login form:
+    $args = array(
+        'redirect' => admin_url(), 
+        'form_id' => 'loginform-custom',
+        'label_username' => __( 'Username custom text' ),
+        'label_password' => __( 'Password custom text' ),
+        'label_remember' => __( 'Remember Me custom text' ),
+        'label_log_in' => __( 'Log In custom text' ),
+        'remember' => true
+    );
+    wp_login_form( $args );
+} else { // If logged in:
+    wp_loginout( home_url() ); // Display "Log Out" link.
+    echo " | ";
+    wp_register('', ''); // Display "Site Admin" link.
+}
+?>
 
 <?php wp_login_form( $args ); ?>
