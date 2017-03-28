1132edc9378642ef709c8e0bb3d80a2c21dd4913
Iris <mdza.iris@gmail.com>
2017-03-28 11:50:59 -0700

    add CFS loop for tough conversations page

--- themes/catching-the-spirit/page-resources-conversations.php
+++ themes/catching-the-spirit/page-resources-conversations.php
@@ -14,13 +14,29 @@ get_header(); ?>
 				<h1 class="page-title"><?php echo CFS()->get('resource_page_title'); ?></h1>
 				<p><?php echo CFS()->get('resource_page_description'); ?></p>
 			</header>
-			<section>
-				<?php while ( have_posts() ) : the_post(); ?>
+			
+			<!--<section>
+            <ul>
+                <?php 
+                    $fields = CFS()->get( 'resource_category_loop' ); ?>
+                <?php foreach ( $fields as $field ): ?>
+                    <li>
+                        <h2><?php echo $field['resource_category_title']; ?></h2>
+                        <p><?php echo $field['resource_link']; ?></p>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </section>-->
 
-					<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php endwhile; // End of the loop. ?>
-			</section>
+		<section>
+			<?php $categories = CFS()->get( 'resource_category_loop'); ?>
+					<?php foreach( $categories as $category ): ?>
+						<h2><?php echo $category[ 'resource_category_title' ]; ?></h2>
+							<?php foreach( $category['resource_link_loop'] as $link): ?>
+								<p><?php echo $link['resource_link']; ?></p>
+							<?php endforeach; ?>
+					<?php endforeach; ?>
+        </section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
