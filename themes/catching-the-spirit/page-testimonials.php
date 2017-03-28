c981951f6388a2598474f4b71dd436ff4463215b
Iris <mdza.iris@gmail.com>
2017-03-28 13:28:41 -0700

    add testimonial archive link and page

--- themes/catching-the-spirit/page-testimonials.php
+++ themes/catching-the-spirit/page-testimonials.php
@@ -19,33 +19,35 @@ get_header(); ?>
             <section class="testimonials">
                 <h2>Here's what other people are saying about us:</h2>
                 <ul>
-                <?php
-                    //global $post;
-                    $args = array( 
-                        'post_type' => 'testimonial',
-                        'posts_per_page' => 4, 
-                        'order'=> 'DSC', 
-                        'orderby' => 'post_date' 
-                    );
-                    $postslist = get_posts( $args );
-                    foreach ( $postslist as $post ):
-                        setup_postdata( $post ); ?> 
-                            <li class="latest-posts">
-                                <div class="post-thumbnail">
+                    <?php
+                        //global $post;
+                        $args = array( 
+                            'post_type' => 'testimonial',
+                            'posts_per_page' => 4, 
+                            'order'=> 'DSC', 
+                            'orderby' => 'post_date' 
+                        );
+                        $postslist = get_posts( $args );
+                        foreach ( $postslist as $post ): setup_postdata( $post ); ?>
+
+                        <li class="latest-posts">
+                            <div class="post-thumbnail">
                                 <?php if ( has_post_thumbnail() ) {
                                     the_post_thumbnail('large');
                                     } else { ?>
                                     <img src="<?php echo CFS()->get('program_image', get_page_by_path('programs/leadership')->ID); ?>" alt="Image of testimonial writer" />
-                                    <?php } ?>
-                                    <h3 class="post-name"><?php the_title(); ?></h3>
-                                </div>                                    
-                                <div class="entry-content">
+                                <?php } ?>
+                                <h3 class="post-name"><?php the_title(); ?></h3>
+                            </div>                                    
+                            <div class="entry-content">
                                 <?php the_content(); ?>
-                                </div><!-- .entry-content -->              
-                            </li> <!-- latest-posts-->
-                <?php endforeach; wp_reset_postdata(); ?>
+                            </div>  
+                        </li><!-- latest-posts -->
+                    <?php endforeach; wp_reset_postdata(); ?>
                 </ul>
-            </section> <!-- testimonials -->
+                <a href="<?php echo esc_url( home_url( '/testimonial' ) ); ?>" class="register-btn">See More</a>
+
+            </section><!-- testimonials -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
