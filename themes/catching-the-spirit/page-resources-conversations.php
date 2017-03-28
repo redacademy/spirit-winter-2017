f4bc4cc9c62c207f0b2c757c58154c7ae949e59a
Iris <mdza.iris@gmail.com>
2017-03-28 12:17:47 -0700

    edit white space in tough conversations code

--- themes/catching-the-spirit/page-resources-conversations.php
+++ themes/catching-the-spirit/page-resources-conversations.php
@@ -14,29 +14,16 @@ get_header(); ?>
 				<h1 class="page-title"><?php echo CFS()->get('resource_page_title'); ?></h1>
 				<p><?php echo CFS()->get('resource_page_description'); ?></p>
 			</header>
-			
-			<!--<section>
-            <ul>
-                <?php 
-                    $fields = CFS()->get( 'resource_category_loop' ); ?>
-                <?php foreach ( $fields as $field ): ?>
-                    <li>
-                        <h2><?php echo $field['resource_category_title']; ?></h2>
-                        <p><?php echo $field['resource_link']; ?></p>
-                    </li>
-                <?php endforeach ?>
-            </ul>
-        </section>-->
 
-		<section>
-			<?php $categories = CFS()->get( 'resource_category_loop'); ?>
-					<?php foreach( $categories as $category ): ?>
-						<h2><?php echo $category[ 'resource_category_title' ]; ?></h2>
-							<?php foreach( $category['resource_link_loop'] as $link): ?>
-								<p><?php echo $link['resource_link']; ?></p>
-							<?php endforeach; ?>
+			<section>
+				<?php $categories = CFS()->get( 'resource_category_loop'); ?>
+				<?php foreach( $categories as $category ): ?>
+					<h2><?php echo $category[ 'resource_category_title' ]; ?></h2>
+					<?php foreach( $category['resource_link_loop'] as $link): ?>
+						<p><?php echo $link['resource_link']; ?></p>
 					<?php endforeach; ?>
-        </section>
+				<?php endforeach; ?>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
