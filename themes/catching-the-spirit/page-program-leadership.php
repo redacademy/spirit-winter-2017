f41da628c145b89862cf640ea0e4ef9c5330e741
Iris <mdza.iris@gmail.com>
2017-03-28 19:00:13 -0700

    sanitize custom fields for programs pages

--- themes/catching-the-spirit/page-program-leadership.php
+++ themes/catching-the-spirit/page-program-leadership.php
@@ -11,9 +11,9 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<header class="program-header">
-				<h1 class="page-title"><?php echo CFS()->get('program_type'); ?></h1>
+				<h1 class="page-title"><?php echo esc_html(CFS()->get('program_type')); ?></h1>
 				<div class="program-header-image">
-                    <img src="<?php echo CFS()->get( 'program_image' ); ?>"/>
+                    <img src="<?php echo esc_html(CFS()->get( 'program_image' )); ?>"/>
                 </div>
 				<span class="program-description"><?php echo CFS()->get('program_description'); ?></span>
 				<a href="" class="blue-btn">Peer Leaders</a> <a href="" class="blue-btn">Supervisors</a>
@@ -26,12 +26,12 @@ get_header(); ?>
 					<div class="program-leaders-wrapper">
 						<div class="peer-leaders">
 							<p>Peer Leaders</p>
-							<div class="leaders-image"><img src="<?php echo CFS()->get('peer_leaders_image'); ?>"/></div>
+							<div class="leaders-image"><img src="<?php echo esc_html(CFS()->get('peer_leaders_image')); ?>"/></div>
 							<p>Age: 15-18</p>
 						</div>
 						<div class="supervisors">
 							<p>Supervisors</p>
-							<div class="leaders-image"><img src="<?php echo CFS()->get('supervisors_image'); ?>"/></div>
+							<div class="leaders-image"><img src="<?php echo esc_html(CFS()->get('supervisors_image')); ?>"/></div>
 							<p>Age: 19+</p>
 						</div>
 					</div>
@@ -43,13 +43,13 @@ get_header(); ?>
 				<div class="green-box peer-leaders-description">
 					<h2>Peer Leaders</h2>
 					<div><?php echo CFS()->get('peer_leaders_description'); ?></div>
-					<a href="<?php echo CFS()->get('apply_peer_leaders'); ?>" class="apply-btn">Apply for peer leaders</a>
+					<a href="<?php echo esc_html(CFS()->get('apply_peer_leaders')); ?>" class="apply-btn">Apply for peer leaders</a>
 				</div>
 
 				<div class="supervisors-description text-container">
 					<h2>Supervisors</h2>
 					<div><?php echo CFS()->get('supervisors_description'); ?></div>
-					<a href="<?php echo CFS()->get('apply_supervisors'); ?>" class="apply-btn">Apply for supervisors</a>
+					<a href="<?php echo esc_html(CFS()->get('apply_supervisors')); ?>" class="apply-btn">Apply for supervisors</a>
 				</div>
 			</section>
 					
--- themes/catching-the-spirit/page-program-seasonal.php
+++ themes/catching-the-spirit/page-program-seasonal.php
@@ -11,15 +11,15 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<header class="program-header">
-				<h1 class="page-title"><?php echo CFS()->get('program_type'); ?></h1>
+				<h1 class="page-title"><?php echo esc_html(CFS()->get('program_type')); ?></h1>
 				<?php 
 					$field = CFS()->get('program_name');
 					if ( !empty( $field ) ) : 
 					?>
-					<span class="subtitle"><?php echo CFS()->get('program_name'); ?></span>
+					<span class="subtitle"><?php echo esc_html($field); ?></span>
 				<?php endif; ?>
 				<div class="program-header-image">
-                    <img src="<?php echo CFS()->get( 'program_image' ); ?>"/>
+                    <img src="<?php echo esc_html(CFS()->get( 'program_image' )); ?>"/>
                 </div>
 			</header>
 
@@ -44,12 +44,12 @@ get_header(); ?>
                     <div class="program-leaders-wrapper">
                         <div class="peer-leaders">
                             <p>Peer Leaders</p>
-                            <img src="<?php echo CFS()->get('peer_leaders_image', get_page_by_path('programs/leadership')->ID); ?>"/>
+                            <img src="<?php echo esc_html(CFS()->get('peer_leaders_image', get_page_by_path('programs/leadership')->ID)); ?>"/>
                             <p>Age: 15-18</p>
                         </div>
                         <div class="supervisors">
                             <p>Supervisors</p>
-                            <img src="<?php echo CFS()->get('supervisors_image', get_page_by_path('programs/leadership')->ID); ?>"/>
+                            <img src="<?php echo esc_html(CFS()->get('supervisors_image', get_page_by_path('programs/leadership')->ID)); ?>"/>
                             <p>Age: 19+</p>
                         </div>
                     </div>
